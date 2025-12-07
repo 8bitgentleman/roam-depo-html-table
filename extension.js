7fbaf6cfd81da1571709fa1451bfaa3c1e2a5868
Matt Vogel <mainmoniker@googlemail.com>
2025-12-07 10:59:27 -0500

    feat:Improve table import and add suggestions doc
    Refactored extension.js to enhance table import functionality: improved error handling, clarified row/column parsing, and added support for both extensionAPI and legacy API.
    
    use async
    
    cleanup bad code

--- extension.js
+++ extension.js
@@ -1,77 +1,75 @@
 /* Original code by matt vogel */
-  /* Source: https://github.com/8bitgentleman/roam-depo-html-table  */
-  /* v1  */
-  // creates a right click menu plugin which will convert a copied HTML table to a roam table
-  // this table was used as reference https://www.w3schools.com/html/html_tables.asp
+/* Source: https://github.com/8bitgentleman/roam-depo-html-table */
+/* v2 */
+// Creates a right click menu command to convert copied TSV/CSV data into a Roam table
 
-  async function pasteTable(e) {
-      // log the context (block uid)
-      // console.log(e);
-      // get the clipboard text
-      const clipText = await navigator.clipboard.readText();
-      // log the clipboard
-      // console.log(clipText);
+async function pasteTable(blockUid) {
+    try {
+        // Get clipboard text
+        const clipText = await navigator.clipboard.readText();
 
-      // split into rows by the ENTER key
-      // clipRows = clipText.split(String.fromCharCode(13));
-      let clipRows = clipText.split(/\r?\n/);
-      // console.log(clipRows);
+        // Split into rows (handle both Windows and Unix line endings)
+        const rows = clipText.split(/\r?\n/);
 
-      // split rows into columns
-      for (let i = 0; i < clipRows.length; i++) {
-          clipRows[i] = clipRows[i].split(String.fromCharCode(9));
-      }
-      // console.log(clipRows);
+        // Split each row into columns (tab-separated)
+        const tableData = rows.map(row => row.split('\t'));
 
-      // 
-      // create the roam table here
-      // 
-      let tableUID = window.roamAlphaAPI.util.generateUID();
-      window.roamAlphaAPI.updateBlock({
-          "block": {
-              "uid": e,
-              "string": "{{[[table]]}}"
-          }
-      })
+        // Update the target block to be a table
+        await window.roamAlphaAPI.data.block.update({
+            block: {
+                uid: blockUid,
+                string: "{{[[table]]}}"
+            }
+        });
 
+        // Create nested blocks for each row
+        async function createRow(parentUid, rowData) {
+            let currentParentUid = parentUid;
 
-      function createRow(e, row) {
+            for (let i = 0; i < rowData.length; i++) {
+                const cellUid = window.roamAlphaAPI.util.generateUID();
 
-          var parentUID = e
-          for (let i = 0; i < row.length; i++) {
-              var uid = window.roamAlphaAPI.util.generateUID();
+                await window.roamAlphaAPI.data.block.create({
+                    location: {
+                        "parent-uid": currentParentUid,
+                        order: "last"
+                    },
+                    block: {
+                        uid: cellUid,
+                        string: rowData[i]
+                    }
+                });
 
-              window.roamAlphaAPI.createBlock({
-                  "location": {
-                      "parent-uid": parentUID,
-                      "order": "last"
-                  },
-                  "block": {
-                      "uid": uid,
-                      "string": row[i]
-                  }
-              })
-              parentUID = uid;
-          }
-      }
-      for (let r = 0; r < clipRows.length; r++) {
-          createRow(e, clipRows[r])
-      }
-  }
+                // Next cell becomes child of current cell (creates nested structure)
+                currentParentUid = cellUid;
+            }
+        }
+
+        // Create all rows sequentially to avoid race conditions
+        for (let i = 0; i < tableData.length; i++) {
+            await createRow(blockUid, tableData[i]);
+        }
+    } catch (error) {
+        console.error("Table Import Error:", error);
+        alert("Failed to paste table. Please check console for details.");
+    }
+}
 
 
 export default {
   onload: () => {
     console.log("load table import plugin")
-    roamAlphaAPI.ui.blockContextMenu.addCommand({
+
+    window.roamAlphaAPI.ui.blockContextMenu.addCommand({
       label: "Paste Table",
       callback: (e) => pasteTable(e['block-uid'])
-  })
+    })
   },
   onunload: () => {
     console.log("unload table import plugin")
-    roamAlphaAPI.ui.blockContextMenu.removeCommand(
-      {label: "Paste Table"}
-    )
+
+    window.roamAlphaAPI.ui.blockContextMenu.removeCommand({
+      label: "Paste Table"
+    })
   }
 };
